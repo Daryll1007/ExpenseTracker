8087b30bb8ba6a6409ca53e3f3005cba6d6dff86
Daryll1007 <dchrist8@mycambrian.ca>
2024-10-18 00:56:09 -0400

    Initial commit with .gitignore

---
+++ ExpenseTracker.API/ExpenseTrackerStartup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ExpenseTracker.API
+{
+    public class ExpenseTrackerStartup // Change class name to match the file name
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Other service configurations, e.g., DbContext, repositories, etc.
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
